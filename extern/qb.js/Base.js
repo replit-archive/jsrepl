a91f05fe06f9a7ba3e26825ba673008f8dbaea2e
max99x <max99x@gmail.com>
2011-09-13 21:01:55 +0300

    Added Closure-preserved QBasic license.

--- extern/qb.js/Base.js
+++ extern/qb.js/Base.js
@@ -1,2 +1,22 @@
+/**
+  @preserve
+  Copyright 2010 Steve Hanov
+
+  This file is part of qb.js
+
+  qb.js is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  qb.js is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with qb.js.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 // Defines the main QBasic object.
 var QBasic = {};
